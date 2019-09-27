308c620739894c51c09a461c612ed1c3e927f6a5
Zheng <zheng.lu@salesforce.com>
2019-09-27 15:15:35 -0700

    update json serialize

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 @app.route('/')
 def search():
     verification_id = request.args.get('id')
-    print(verification_id)
     res = core.search(verification_id)
 
-    return json.dumps(res)
+    return json.dumps(res, indent=4, sort_keys=False, default=str)
